d786d6d612fb48ba9e0b85e3d6ae835acbf14301
MD. Mahmudul Hasan <61178414+mahmudulri@users.noreply.github.com>
2023-05-12 14:56:27 +0600

    adsfs

--- lib/api_page.dart
+++ lib/api_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
-
 import 'controller.dart';
 
 class ApiPage extends StatelessWidget {
@@ -13,25 +11,39 @@ class ApiPage extends StatelessWidget {
     PostController postController = Get.put(PostController());
     return SafeArea(
         child: Scaffold(
-      body: postController.isloading.value
-          ? CircularProgressIndicator()
-          : ListView.builder(
-              itemCount: postController.allpost.length,
-              itemBuilder: (context, index) {
-                return Column(
-                  children: [
-                    Text(postController.allpost[index].title),
-                    Container(
-                      height: 150,
-                      width: 150,
-                      child: Image.network(
-                        postController.allpost[index].thumbnail,
-                      ),
-                    ),
-                  ],
-                );
-              },
-            ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: StaggeredGridView.countBuilder(
+          staggeredTileBuilder: (index) => index % 8 == 0
+              ? StaggeredTile.count(1, 1)
+              : StaggeredTile.count(2, 1),
+          mainAxisSpacing: 8,
+          crossAxisSpacing: 8,
+          crossAxisCount: 3,
+          itemCount: postController.allpost.length,
+          itemBuilder: (context, index) {
+            return Card(
+              margin: EdgeInsets.zero,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Container(
+                margin: EdgeInsets.all(8),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Container(
+                    color: Colors.red,
+                  ),
+                  // child: Image.network(
+                  //   postController.allpost[index].thumbnail,
+                  //   fit: BoxFit.cover,
+                  // ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     ));
   }
 }
